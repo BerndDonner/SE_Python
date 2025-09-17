2642d168e3561891bbc00613f1ef3226d67fcd42
Donner Bernd <bernd.donner@GBS.sabel.local>
2025-09-17 16:29:26 +0200

    Datentypen, Strings, f-Strings

---
+++ Datentypen/main.py
@@ -0,0 +1,20 @@
+# Dies ist mein erstes python programm
+
+a = 1
+print(a)
+print(type(a))
+
+pi = 3.1415
+print(pi)
+print(type(pi))
+
+b = 'Brutus: "Sag pi =', pi, 'oder stirb!"'
+print(b)
+
+
+# print(string, float, string)
+print('Brutus: "Sag pi =', pi, 'oder stirb!"')
+print('Brutus: "Sag pi = ' + str(pi) + ' oder stirb!"')
+
+c = f'Brutus: "Sag pi = {pi} oder stirb!"'
+print(c)
