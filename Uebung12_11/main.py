c851e9ea8a3928f8c78d93c3e4b7b3bebf2ca9c2
Bernd Donner <bernd.donner@sabel.com>
2025-11-12 17:20:30 +0100

    Unterricht von 12.11

---
+++ Uebung12_11/main.py
@@ -0,0 +1,36 @@
+import random
+
+karte_vorne = ("🐍", "🐢", "🐸")
+karte_hinten = ("🟦", "🟥")
+
+kombis = []
+for hinten in karte_hinten:
+    for vorne in karte_vorne:
+        kombis.append((hinten, vorne))
+
+random.shuffle(kombis)
+
+
+
+karten = [tupel[0] for tupel in kombis]
+print("Karten: ", *karten)
+print("Index:  ", end=" ")
+for i in range(len(karten)):
+    print(f"{i:2}", end=" ")
+
+
+
+
+i, j = input("Welche zwei Karten möchten Sie aufdecken (z. B. 0 1)? ").split()
+
+i = int(i)
+j = int(j)
+
+if i < 0 or i >= len(karten) or j < 0 or j >= len(karten) or i == j:
+    print("Ungültige Indizes.")
+
+a, b = kombis[i]
+kombis[i] = (b, a)
+
+a, b = kombis[j]
+kombis[j] = (b, a)
