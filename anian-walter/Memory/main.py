471e3be98600254a292dfff9baf0e0a68a0991db
anian-walter <anian.walter02@gmail.com>
2025-12-03 15:48:38 +0100

    Memory

--- anian-walter/Memory/main.py
+++ anian-walter/Memory/main.py
@@ -1,47 +1,52 @@
 import random
+def main():
+    def spielfeld():
+        karten = [tupel[0] for tupel in kombis]
+        print("Karten: ", *karten)
+        print("Index:  ", end=" ")
+        for i in range(len(karten)):
+            print(f"{i:2}", end=" ")
+        print("\n")
 
-def spielfeld():
-    karten = [tupel[0] for tupel in kombis]
-    print("Karten: ", *karten)
-    print("Index:  ", end=" ")
-    for i in range(len(karten)):
-        print(f"{i:2}", end=" ")
-    print("\n")
+    def karten_lesen_und_vergleichen():
+        i, j = input("Welche zwei Karten möchten Sie aufdecken (z. B. 0 1)? ").split()
+        i = int(i)
+        j = int(j)
+        if i >= 0 and i < len(kombis) and j >= 0 and j < len(kombis) and j != i: 
+            a, b = kombis[i]
+            kombis[i] = (b, a)
+            a, b = kombis[j]
+            kombis[j] = (b, a)
+        else:
+            print("Ungültige Indizes.")
+            karten_lesen_und_vergleichen()
 
-def karten_lesen_und_vergleichen():
-    i, j = input("Welche zwei Karten möchten Sie aufdecken (z. B. 0 1)? ").split()
-    i = int(i)
-    j = int(j)
-    if i < 0 or i >= len(kombis) or j < 0 or j >= len(kombis) or i == j or kombis[i] == karte_vorne or kombis[j] == karte_vorne:
-        print("Ungültige Indizes.")
-    else:
-        a, b = kombis[i]
-        kombis[i] = (b, a)
-        a, b = kombis[j]
-        kombis[j] = (b, a)
+        spielfeld()
 
-    spielfeld()
+        if kombis[i][0] == kombis[j][0]:
+            print("Treffer!")
+        else:
+            print("Leider kein Treffer.")   
+            a, b = kombis[i]
+            kombis[i] = (b, a)      
+            a, b = kombis[j]
+            kombis[j] = (b, a)
 
-    if kombis[i][0] == kombis[j][0]:
-        print("Treffer!")
-    else:
-        print("Leider kein Treffer.")   
-        a, b = kombis[i]
-        kombis[i] = (b, a)      
-        a, b = kombis[j]
-        kombis[j] = (b, a)
 
-karte_vorne = ("🐍", "🐢", "🐸")
-karte_hinten  = ("🟦", "🟥")
+    karte_vorne = ("🐍", "🐢", "🐸")
+    karte_hinten  = ("🟦", "🟥")
 
-kombis = []
-for hinten in karte_hinten:
-    for vorne in karte_vorne:
-        kombis.append((hinten, vorne))    
+    kombis = []
+    for hinten in karte_hinten:
+        for vorne in karte_vorne:
+            kombis.append((hinten, vorne))    
 
-random.shuffle(kombis)
+    random.shuffle(kombis)
 
-while any(kartenseite in karte_hinten for kartenseite in [tupel[0] for tupel in kombis]):
-    spielfeld()
-    karten_lesen_und_vergleichen()
-print("Glückwunsch, alle Paare gefunden!")
+    while any(kartenseite in karte_hinten for kartenseite in [tupel[0] for tupel in kombis]):
+        spielfeld()
+        karten_lesen_und_vergleichen()
+    print("Glückwunsch, alle Paare gefunden!")
+    if "1" == input("Drücke 1 um neu zu starten."):
+        main()
+main()
